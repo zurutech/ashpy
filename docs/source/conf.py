67d6b651a1f32c350f5fd42b1924a2f8e205c9e8
Paolo Galeone <paolo@zuru.tech>
2019-07-19 11:56:43 +0200

    custom colors in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -139,6 +139,8 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_css_files = ["css/custom.css"]
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
