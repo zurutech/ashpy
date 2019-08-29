f1a99bc1086fb9d3c4b085b92fe6972aa7be5ed7
emanuele <emanuele@zuru.tech>
2019-08-29 16:55:02 +0200

    SSIM_Multiscale: add license and documentation

--- ashpy/metrics/ssim_multiscale.py
+++ ashpy/metrics/ssim_multiscale.py
@@ -1,3 +1,17 @@
+# Copyright 2019 Zuru Tech HK Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 SlicedWasserseinDistance metric.
 """
@@ -18,7 +32,14 @@ if TYPE_CHECKING:
 
 
 class SSIM_Multiscale(Metric):  # pylint: disable=invalid-name
-    """Multiscale Structural Similarity"""
+    r"""
+    Multiscale Structural Similarity
+
+    See Multiscale structural similarity for image quality assessment [1]_
+
+    .. [1] Multiscale structural similarity for image quality assessment
+        https://ieeexplore.ieee.org/document/1292216
+    """
 
     def __init__(
         self,
