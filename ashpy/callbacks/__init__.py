6005da2cead81c5d40ef71373cd8544bdb54d2bd
emanuele <emanuele@zuru.tech>
2019-09-30 12:42:21 +0200

    Callback implementation

---
+++ ashpy/callbacks/__init__.py
@@ -0,0 +1,43 @@
+# Copyright 2019 Zuru Tech HK Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Callbacks in order to gain control over the training loop.
+
+.. currentmodule:: ashpy.callbacks
+
+.. rubric:: Classes
+
+.. autosummary::
+    :nosignatures:
+    :toctree: callbacks
+
+    callback.Callback
+
+----
+
+.. rubric:: Modules
+
+.. autosummary::
+    :nosignatures:
+    :toctree: contexts
+    :template: autosummary/submodule.rst
+
+    callback
+
+"""
+
+from ashpy.callbacks.callback import Callback
+
+__ALL__ = ["Callback"]
---
+++ ashpy/callbacks/callback.py
@@ -0,0 +1,91 @@
+# Copyright 2019 Zuru Tech HK Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Callback definition
+"""
+
+from ashpy.contexts import BaseContext
+
+
+class Callback:
+    """
+    Callback definition.
+    Every callback must extend from this class.
+    This class defines the basic events.
+    Every event takes as input the context in order to use the objects defined.
+
+    Order:
+    --on_train_start
+    ----on_epoch_start
+    ------on_batch_start
+    ------on_batch_end
+    ----on_epoch_end
+    --on_train_end
+
+    on_exception – if an Exception was raised
+
+    """
+
+    def __init__(self):
+        pass
+
+    def on_train_start(self, context: BaseContext) -> None:
+        """
+        Method called at the beginning of the training loop
+        Args:
+            context: training context
+        """
+
+    def on_train_end(self, context: BaseContext) -> None:
+        """
+        Method called at the end of the training loop
+        Args:
+            context: training context
+        """
+
+    def on_epoch_start(self, context: BaseContext) -> None:
+        """
+        Method called at the beginning of an epoch
+        Args:
+            context: training context
+        """
+
+    def on_epoch_end(self, context: BaseContext) -> None:
+        """
+        Method called at the end of an epoch
+        Args:
+            context: training context
+        """
+
+    def on_batch_start(self, context: BaseContext) -> None:
+        """
+        Method called at the beginning of a batch
+        Args:
+            context: training context
+        """
+
+    def on_batch_end(self, context: BaseContext) -> None:
+        """
+        Method called at the end of a batch
+        Args:
+            context: training context
+        """
+
+    def on_exception(self, context: BaseContext) -> None:
+        """
+        Method called when an exception is raised
+        Args:
+            context: training context
+        """
